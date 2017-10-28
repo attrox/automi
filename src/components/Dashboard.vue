1b969158624f301c0451d6763bdb2d219c025e3e
Ricky Leman <rickyleman@gmail.com>
2017-10-28 09:39:19 -0700

    fix linting error

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,54 +2,38 @@
   <div class="dashboard">
     <h1>{{ msg }}</h1>
     <div>
-      <md-layout class="card">
-      <md-layout></md-layout>
-      <md-layout md-flex="50">
-        <md-card md-with-hover>
-          <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-          </md-card-content>
-          <md-card-actions>
-            <md-button class="md-icon-button">
-              <md-icon>favorite</md-icon>
-            </md-button> 200
-          </md-card-actions>
-        </md-card>
-      </md-layout>
-      <md-layout></md-layout>
-      </md-layout>
-      <md-layout class="card">
-      <md-layout></md-layout>
-      <md-layout md-flex="50">
-        <md-card md-with-hover>
-          <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-          </md-card-content>
-          <md-card-actions>
-            <md-button class="md-icon-button">
-              <md-icon>favorite</md-icon>
-            </md-button> 200
-          </md-card-actions>
-        </md-card>
-      </md-layout>
-      <md-layout></md-layout>
-      </md-layout>
-      <md-layout class="card">
-      <md-layout></md-layout>
-      <md-layout md-flex="50">
-        <md-card md-with-hover>
-          <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-          </md-card-content>
-          <md-card-actions>
-            <md-button class="md-icon-button">
-              <md-icon>favorite</md-icon>
-            </md-button> 200
-          </md-card-actions>
-        </md-card>
-      </md-layout>
-      <md-layout></md-layout>
-      </md-layout>
+        <md-layout class="card">
+            <md-layout></md-layout>
+            <md-layout md-flex="50">
+                <md-card md-with-hover>
+                <md-card-content>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </md-card-content>
+                <md-card-actions>
+                    <md-button class="md-icon-button">
+                    <md-icon>favorite</md-icon>
+                    </md-button> 200
+                </md-card-actions>
+                </md-card>
+            </md-layout>
+            <md-layout></md-layout>
+        </md-layout>
+        <md-layout class="card">
+            <md-layout></md-layout>
+            <md-layout md-flex="50">
+                <md-card md-with-hover>
+                <md-card-content>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </md-card-content>
+                <md-card-actions>
+                    <md-button class="md-icon-button">
+                    <md-icon>favorite</md-icon>
+                    </md-button> 200
+                </md-card-actions>
+                </md-card>
+            </md-layout>
+            <md-layout></md-layout>
+        </md-layout>
     </div>
     <div v-if="userExists">
       Welcome {{ pseudo }}. Destroy your account by clicking <a href="#" @click="destroyAccount">here</a>.
@@ -71,30 +55,34 @@ export default {
     },
     computed: {
         userExists: function () {
-            return (typeof this.pseudo !== 'undefined')
+            return typeof this.pseudo !== 'undefined'
         }
     },
     beforeCreate: function () {
-        Users.init().then(() => {
-            Users.exists(window.web3.eth.accounts[0]).then((exists) => {
-                if (exists) {
-                    Users.authenticate().then(pseudo => {
-                        this.pseudo = pseudo
-                    })
-                }
-            })
-        }).catch(err => {
-            console.log(err)
-        })
+        Users.init()
+      .then(() => {
+          Users.exists(window.web3.eth.accounts[0]).then(exists => {
+              if (exists) {
+                  Users.authenticate().then(pseudo => {
+                      this.pseudo = pseudo
+                  })
+              }
+          })
+      })
+      .catch(err => {
+          console.log(err)
+      })
     },
     methods: {
         destroyAccount: function (e) {
             e.preventDefault()
-            Users.destroy().then(() => {
-                this.pseudo = undefined
-            }).catch(err => {
-                console.log(err)
-            })
+            Users.destroy()
+        .then(() => {
+            this.pseudo = undefined
+        })
+        .catch(err => {
+            console.log(err)
+        })
         }
     }
 }
@@ -102,7 +90,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
   display: block;
 }
